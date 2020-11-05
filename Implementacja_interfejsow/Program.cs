66d18306648c1c67813619bb92303cd766f825a8
AgnieszkaJania <65187859+AgnieszkaJania@users.noreply.github.com>
2020-11-05 20:50:15 +0100

    Dodaj pliki projektów.

---
+++ Implementacja_interfejsow/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Implementacja_interfejsow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello World!");
+            string x = " Dzien ";
+            Console.WriteLine(x.Trim());
+        }
+    }
+}
